467c7ce79e589f39f79a5090279f30a0f24efb2d
Khosman23 <khalidho@hiof.no>
2025-10-11 15:02:11 +0200

    Byttet ut App logo (bilde) med GIF. Lagt til kommentarer i koden også

--- ChoreChamp/app/(onboarding)/OnboardingScreen.tsx
+++ ChoreChamp/app/(onboarding)/OnboardingScreen.tsx
@@ -1,8 +1,13 @@
 import { StyleSheet, View, Image, Text, Animated } from 'react-native';
 import React, { useEffect, useRef } from 'react';
 import { useRouter } from 'expo-router';
-import { replace } from 'expo-router/build/global-state/routing';
 
+/*
+Onboarding Screen
+    - Displays the app logo when the user first opens the app.
+    - The logo is an animated gif and moves upward as the app fades in.
+    - After the animation finishes, the user is navigated to the startup screen.
+*/
 
 const OnboardingScreen = () => {
     const router = useRouter();
@@ -10,15 +15,16 @@ const OnboardingScreen = () => {
     const opacity = useRef(new Animated.Value(1)).current;
     
     useEffect(() => {
-        const INITIAL_DELAY = 500; 
+        const INITIAL_DELAY = 1500; // Delay before starting the animation in MS
 
-        const ANIM_DURATION = 1500; 
+        const ANIM_DURATION = 3000; // Duration of the animation in MS
 
         let navTimer: ReturnType<typeof setTimeout> | undefined; 
 
+        // Start the animation after delay 
         const delayTimer = setTimeout(() => { 
             Animated.timing(translateY, {
-                toValue: -800,
+                toValue: -500,
                 duration: ANIM_DURATION,
                 useNativeDriver: true,
             }).start();
@@ -29,10 +35,11 @@ const OnboardingScreen = () => {
                 useNativeDriver: true,
             }).start();
 
+            // Navigate to the main app screen after animation
             navTimer = setTimeout(() => { 
                 router.replace('/(tabs)');
             }, ANIM_DURATION + 300); 
-        }, INITIAL_DELAY); 
+        }, INITIAL_DELAY);  
 
         return () => {
             clearTimeout(delayTimer); 
@@ -45,7 +52,7 @@ const OnboardingScreen = () => {
   return (
     <View style={styles.container}>
         <Animated.Image
-            source={require('../../assets/images/Logo.png')}
+            source={require('../../assets/images/Logo.gif')}
             style = {[styles.logo, { transform: [{ translateY }], opacity }]}
             resizeMode= "contain"
         />
@@ -63,8 +70,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFB40E',
     },
     logo: {
-        width: 300,
-        height: 300,
+        width: 400,
+        height: 400,
         marginBottom: 50,
     },
 });
