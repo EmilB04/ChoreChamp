b6a162b363db89a20a89197c38a5e31a33b4d5b2
Andolaus <andolaus@gmail.com>
2025-10-14 11:47:08 +0200

    Lagde firebase.ts

---
+++ ChoreChamp/lib/firebase.ts
@@ -0,0 +1,18 @@
+// lib/firebase.ts
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_bhFiyDRwsswKsxgsBKQVP20O68oKOSs",
+  authDomain: "chorechamp-a000a.firebaseapp.com",
+  projectId: "chorechamp-a000a",
+  // storageBucket: "chorechamp-a000a.appspot.com", 
+  appId: "1:958373042769:web:eca54d4aa785681900f670",
+  // measurementId: "G-3GY9VYLJHE",
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
