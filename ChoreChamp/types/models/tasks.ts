7883b8a68ad6dfebd20b252273df9a4c8c36f2b9
Emil Berglund <emil.berglund@live.no>
2025-10-23 11:55:10 +0200

    Refactor: Remove NPM and force PNPM.

--- ChoreChamp/types/models/tasks.ts
+++ ChoreChamp/types/models/tasks.ts
@@ -1,8 +1,11 @@
 export type Task = {
+    assignedTo: string; // Reference to /collection/users
     createdBy: string;
     description: string;
     householdId: string;
     points: number;
-    status: 'pending' | 'in progress' | 'completed';
+    status: 'not done' | 'in progress' | 'completed';
+    timeEnd: Date;
+    timeStart: Date;
     title: string;
 }
